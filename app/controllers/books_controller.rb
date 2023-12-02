7c5fc5ed67e2151943c129973b5f9df1278b55e9
EC2 Default User <ec2-user@ip-172-31-39-82.ap-northeast-1.compute.internal>
2023-12-02 02:31:10 +0000

    Initial Commit1

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,65 @@
+class BooksController < ApplicationController
+  def new
+    @book = Book.new
+  end
+
+  def view
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+      flash[:notice] = "Book was successfully created"
+      redirect_to book_path(@book.id)
+    else
+      flash[:notice] =""
+      # render :new
+      # redirect_to request.referer
+      @books = Book.all
+      render :index
+    end
+  end
+
+  def index
+    @books = Book.all
+    # @books = Book.all.order(created_at: :desc)
+    @book = Book.new
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    # book = Book.find(params[:id])
+    # book.update(book_params)
+    # redirect_to book_path(book.id)
+    @book = Book.find(params[:id])
+      if @book.update(book_params)
+        flash[:notice] = "Book was successfully created"
+        redirect_to book_path(@book.id)
+      else
+        # flash.now[:notice] = "Ann error has occurred"
+        render :edit
+      end
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    flash[:notice] = "Book was successfully destroyed."
+    redirect_to '/books'
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
+end
